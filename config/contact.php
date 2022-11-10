1d15b7ecfa403bfa35d3e511f6af1e0a0d1feaa6
WanDev <71076451+alwanfa@users.noreply.github.com>
2022-11-10 14:30:33 +0700

    add publish commit

---
+++ config/contact.php
@@ -0,0 +1,4 @@
+<?php
+return[
+    'send_email_to'=>"alwanfauzulazhim15@gmail.com"
+];
--- package/contact/src/ContactServiceProvider.php
+++ package/contact/src/ContactServiceProvider.php
@@ -12,8 +12,32 @@ class ContactServiceProvider extends ServiceProvider{
             'contact'
         );
         $this->publishes([
-            __DIR__.'config/contact.php' => config_path('contact.php'),
+            __DIR__.'/config/contact.php' => config_path('contact.php')
         ]);
+        $this->publishes([
+            __DIR__.'/public/contact.css' => public_path('vendor/contact/contact.css'),
+        ], 'public');
+
+        $this->publishes([
+            __DIR__.'/views/contact' => resource_path('views/contact/contact'),
+        ], 'public');
+
+        $this->publishes([
+            __DIR__.'/views/contact.blade.php' => resource_path('views/contact/contactView.blade.php'),
+        ], 'public');
+
+        $this->publishes([
+            __DIR__.'/database/migrations/2022_10_27_094034_create_contacts_table.php' => app_path('/database/migrations/2022_10_27_094034_create_contacts_table.php'),
+        ], 'public');
+        $this->publishes([
+            __DIR__.'/Models/contact.php' => app_path('/Models/Contact.php'),
+        ], 'public');
+        $this->publishes([
+            __DIR__.'/Http/Controllers/ContactController.php' => app_path('/Http/Controllers/ContactController.php'),
+        ], 'public');
+        $this->publishes([
+            __DIR__.'/Mail' => app_path('/Mail'),
+        ], 'public');        
     }
     public function register()
     {
